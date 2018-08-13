185c3b01397c7f5a82d28adf1529fe3f1198361f
Lmbp <leichen070@gmail.com>
2018-08-13 17:18:38 +0800

    提供datasource获取，并且改进代码

--- src/main/java/com/cl/common/utils/JDBCUtilHikariCP.java
+++ src/main/java/com/cl/common/utils/JDBCUtilHikariCP.java
@@ -24,50 +24,65 @@ public class JDBCUtilHikariCP {
         try{
             properties.load(JDBCUtilDbcp.class.getClassLoader().getResourceAsStream("jdbc.properties"));
             hikariDataSource=new HikariDataSource();
-            hikariDataSource.setDriverClassName((String) properties.get("driverClassName"));
-            hikariDataSource.setJdbcUrl((String) properties.get("url"));
-            hikariDataSource.setUsername((String) properties.get("username"));
-            hikariDataSource.setPassword((String) properties.get("password"));
+            hikariDataSource.setDriverClassName(properties.getProperty("driverClassName"));
+            hikariDataSource.setJdbcUrl(properties.getProperty("url"));
+            hikariDataSource.setUsername(properties.getProperty("username"));
+            hikariDataSource.setPassword(properties.getProperty("password"));
 //            hikariDataSource.setMaximumPoolSize(10);
-            hikariDataSource.setConnectionTestQuery((String) properties.get("conTestQuery"));
+            hikariDataSource.setConnectionTestQuery(properties.getProperty("conTestQuery"));
             queryRunner = new QueryRunner(hikariDataSource);
 
             hikariDataSource2=new HikariDataSource();
-            hikariDataSource2.setDriverClassName((String) properties.get("driverClassName2"));
-            hikariDataSource2.setJdbcUrl((String) properties.get("url2"));
-            hikariDataSource2.setUsername((String) properties.get("username2"));
-            hikariDataSource2.setPassword((String) properties.get("password2"));
+            hikariDataSource2.setDriverClassName(properties.getProperty("driverClassName2"));
+            hikariDataSource2.setJdbcUrl(properties.getProperty("url2"));
+            hikariDataSource2.setUsername(properties.getProperty("username2"));
+            hikariDataSource2.setPassword(properties.getProperty("password2"));
 //            hikariDataSource.setMaximumPoolSize(10);
-            hikariDataSource2.setConnectionTestQuery((String) properties.get("conTestQuery2"));
+            hikariDataSource2.setConnectionTestQuery(properties.getProperty("conTestQuery2"));
             queryRunner2=new QueryRunner(hikariDataSource2);
 
             hikariDataSource3=new HikariDataSource();
-            hikariDataSource3.setDriverClassName((String) properties.get("driverClassName3"));
-            hikariDataSource3.setJdbcUrl((String) properties.get("url3"));
-            hikariDataSource3.setUsername((String) properties.get("username3"));
-            hikariDataSource3.setPassword((String) properties.get("password3"));
+            hikariDataSource3.setDriverClassName(properties.getProperty("driverClassName3"));
+            hikariDataSource3.setJdbcUrl(properties.getProperty("url3"));
+            hikariDataSource3.setUsername(properties.getProperty("username3"));
+            hikariDataSource3.setPassword(properties.getProperty("password3"));
 //            hikariDataSource.setMaximumPoolSize(10);
-            hikariDataSource3.setConnectionTestQuery((String) properties.get("conTestQuery3"));
+            hikariDataSource3.setConnectionTestQuery(properties.getProperty("conTestQuery3"));
+
             queryRunner3=new QueryRunner(hikariDataSource3);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static HikariDataSource getHikariDataSource(){
+        return hikariDataSource;
+    }
+
+    public static HikariDataSource getHikariDataSource2(){
+        return hikariDataSource2;
+    }
+
+    public static HikariDataSource getHikariDataSource3(){
+        return hikariDataSource3;
+    }
+
     public static QueryRunner getQueryRunner(){
         return queryRunner;
-    }
-    public static Connection getConnection() throws SQLException {
-        return hikariDataSource.getConnection();
     }
     public static QueryRunner getQueryRunner2(){
         return queryRunner2;
     }
-    public static Connection getConnection2() throws SQLException {
-        return hikariDataSource2.getConnection();
-    }
     public static QueryRunner getQueryRunner3(){
         return queryRunner3;
+    }
+
+
+    public static Connection getConnection() throws SQLException {
+        return hikariDataSource.getConnection();
+    }
+    public static Connection getConnection2() throws SQLException {
+        return hikariDataSource2.getConnection();
     }
     public static Connection getConnection3() throws SQLException {
         return hikariDataSource3.getConnection();
